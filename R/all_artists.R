2f272573ec49a6bbbce971433fd11c209b8d098c
Sarah Lam <sdlam@bren.ucsb.edu>
2023-03-17 10:00:38 -0700

    updated/clarified data descriptions

--- R/all_artists.R
+++ R/all_artists.R
@@ -1,6 +1,6 @@
 #' All Artists
 #'
-#' All unique artists in 'billboard' data. Includes features as unique artists.
+#' Contains all unique artists in 'billboard' data for a user to search artists function will run on. Includes features as unique artists.
 #' @format A vector of strings.
 #' \itemize{
 #' \item Artist name
